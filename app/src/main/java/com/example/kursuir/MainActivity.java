6658c847e2596bc97536dd7513e82237989d5bed
Maxim PAvlinov <maks0902@gmail.com>
2022-11-07 10:22:44 +0300

    up 07.01 v.3

--- app/src/main/java/com/example/kursuir/MainActivity.java
+++ app/src/main/java/com/example/kursuir/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kursuir;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -7,10 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.kursuir.models.User;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
@@ -49,6 +52,7 @@ RelativeLayout root;
         users = db.getReference("users");
 
         reg.setOnClickListener(v -> showRegisterWindow());
+        log.setOnClickListener(v -> showSingInWindow());
     }
     private void showSingInWindow(){
         root = findViewById(R.id.root);
@@ -56,12 +60,11 @@ RelativeLayout root;
         dialog.setTitle("Register");
         dialog.setMessage("Put all data");
         LayoutInflater inflater = LayoutInflater.from(this);
-        View register_window = inflater.inflate(R.layout.sing_up_window, null);
-        dialog.setView(register_window);
+        View sing_inWindow = inflater.inflate(R.layout.sing_in_winow, null);
+        dialog.setView(sing_inWindow);
 
-        MaterialEditText login = register_window.findViewById(R.id.LoginText);
-        MaterialEditText pass = register_window.findViewById(R.id.PassText);
-        MaterialEditText passconf = register_window.findViewById(R.id.ConfPassText);
+        MaterialEditText login = sing_inWindow.findViewById(R.id.LoginText);
+        MaterialEditText pass = sing_inWindow.findViewById(R.id.PassText);
 
         dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
@@ -83,6 +86,19 @@ RelativeLayout root;
 
             }
         });
+        auth.signInWithEmailAndPassword(login.getText().toString(),pass.getText().toString())
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        startActivity(new Intent(MainActivity.this, Map.class));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Snackbar.make(root,"Incorrect data!" + e.getMessage(),Snackbar.LENGTH_SHORT).show();
+                    }
+                });
         dialog.show();
     }
 
@@ -131,7 +147,7 @@ RelativeLayout root;
                                 user.setLogin(login.getText().toString());
                                 user.setLogin(pass.getText().toString());
 
-                                users.child(user.getLogin())
+                                users.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
                                         .setValue(user)
                                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                                     @Override
